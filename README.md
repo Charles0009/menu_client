42ab1d9f8a8dee4d0380c156190fd45d5e4161f9
Charles0009 <charles.laverdure02@gmail.com>
2022-02-14 09:38:33 +0000

    readme lines added + test implementation

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ To add a menu in the server  :
 add-menu NOM_DU_PLAT,INGREDIENT1,INGREDIENT2,INGREDIENT,... 
 
 exemple : add-menu raclette,fromage,patate,jambon,saucisson
+
+
+To delete a menu in the server :
+
+delete-menu ID_DU_MENU
+
+exemple : delete-menu 1
--- src/test/java/com/cicdlectures/menuclient/ClientControllerTest.java
+++ src/test/java/com/cicdlectures/menuclient/ClientControllerTest.java
@@ -1,5 +1,29 @@
 package com.cicdlectures.menuclient;
 
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import com.cicdlectures.menuclient.controller.ClientController;
+
 public class ClientControllerTest {
     
+
+    CommandLine cmd;
+
+
+    @Test
+    @DisplayName("delete a menu")
+    public void Testdelete() {
+        int exitCode = cmd.execute("delete-menu", "1");
+        assertEquals(200, exitCode);
+    }
+
+
+
 }
